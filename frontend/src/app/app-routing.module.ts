1d9db2f53bd0e9f173fdcb505da7ba2039bbb45c
LM-Inocentes <louisemartininocentes@gmail.com>
2022-09-27 19:07:41 +0800

    login-signup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { LoginComponent } from './login/login.component';
 import { SeatsComponent } from './seats/seats.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
     {path : '', component : LandingpageComponent},
     {path : 'login', component : LoginComponent},
-
+    {path : 'sign-up', component : SignupComponent},
   ];
 
 // configures NgModule imports and exports
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SeatsComponent } from './seats/seats.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SeatsComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
