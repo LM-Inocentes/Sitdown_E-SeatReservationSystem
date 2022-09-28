6dda336b354c8754fa066bb33bf25ab171e0b32c
LM-Inocentes <louisemartininocentes@gmail.com>
2022-09-28 22:59:18 +0800

    added data in backend

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,24 +1,13 @@
 
-export class SeatsInfoService {
-    ImgSeat: string = './assets/Available.png';
-    seatString: string = '';
-    i: number = 1;
-    j: number = 1;
-    TotalSeats: number = 50;
-    Col: number = 10;
-  
-    seats:any[] = [];
-   
-    constructor() { }
-  
-    getSeats():any[]{
-      var pushseats:any;
-     
-      for(; this.j<=this.TotalSeats; this.j++){
-          pushseats = {TotalSeats: this.TotalSeats, Col: this.Col, SeatNo: this.j+((this.i-1)*this.Col), isAvailable: true, img:this.ImgSeat, Name:'' , ReservedDate: new Date(), imgPayment:''}; 
-          this.seats.push(pushseats); 
-          console.log(this.seats[this.j-1]);
-      }
-      return this.seats;
+
+export const sample_events: any[] = [
+    {
+        EventName: "Concert",
+        EventLocation: "Cebu City",
+        EventAbout: "Sample Event About",
+        TotalSeats: 100,
+        Col: 10,
+        Price: 500
     }
-  }
+]
+
